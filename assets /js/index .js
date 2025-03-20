b374b85ebf6d4fb91d5d8090771bb0a7ac8eca72
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-03-20 10:42:23 +0100

    section3

--- assets /js/index .js
+++ assets /js/index .js
@@ -1,11 +1,24 @@
-//ACTIVER L'ANIMMATION EN CLIQUANT SUR UN BOUTON
-
-// LANCER L'ANIMATION AVEC CLICK
 document.getElementById('animateButton').addEventListener('click', function() {
-    var element = document.getElementById('myElement');
-    element.classList.toggle('show');
+    let element = document.getElementById('myElement');
+    element.classList.toggle('show');  // Affiche ou cache l'élément
 });
-//FAIRE DISPARAÎTRE L'ÉLEMENT AVEC UN CLICK
-element.addEventListener('transitionend', function() {
-    element.classList.add('hide');
+
+// Utilisation de transitionend pour ajouter la classe 'hide' après la fin de la transition
+let element = document.getElementById('myElement');
+element.addEventListener('click', function() {
+    element.classList.add('hide');  // Cache l'élément après la fin de la transition
+});
+
+// Partie 2 : Animer le déplacement de l'élément vers la gauche au clic
+
+let object = document.getElementById("myElementRight");
+let button = document.getElementById("animateButtonRight");
+
+button.addEventListener("click", function() {
+    object.classList.toggle('showRight');  // Déplace l'élément vers la gauche
+});
+
+// Lorsque la transition est terminée, on ajoute la classe 'hideLeft' pour rendre l'élément invisible
+object.addEventListener('click', function() {
+    object.classList.add('hideRight');  // Cache l'élément après son déplacement
 });
